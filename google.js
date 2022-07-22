c1ed2458b202a400e54607a706d8e97094bcc3b3
Muhammad Arslan Riaz <marslan.riaz@systemsltd.com>
2022-07-22 12:46:43 +0500

    main: added google event import

--- google.js
+++ google.js
@@ -4,14 +4,37 @@ import axios from 'axios';
 
 const router = Router();
 
-router.post('/', (req,res) => {
+router.post('/', async  (req,res) => {
   const {accessKey}  = req.body;
   console.info('ree', req.body)
   if(!accessKey)
   return res.send('Invalid Access key');
 
   // import all the events of the google
-  res.send('hi');
+  try {
+    const OAuth2 = google.auth.OAuth2;
+    const oauth2Client = new OAuth2(
+      process.env.GOOGLE_CLIENT_ID,
+      process.env.GOOGLE_CLIENT_SECRET,
+    );
+    oauth2Client.setCredentials({
+      access_token: accessKey,
+    });
+    const calendar = google.calendar({ version: 'v3', auth: oauth2Client });
+    const fetchedEvents = await calendar.events.list({
+      calendarId: 'primary',
+      timeZone: 'UTC',
+    });
+    return res.json({
+      data: fetchedEvents
+    });
+  } catch (error) {
+    res.json({
+      data: error
+    });
+  }
+
+  // res.send('hi');
 })
 
 export default router;
